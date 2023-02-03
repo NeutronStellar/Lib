238eecb9725bd10d0aa03aeb635cf802a8a62907
NeutronStellar <93152199+NeutronStellar@users.noreply.github.com>
2023-02-03 10:07:08 -0700

    Update STDC.h

--- STDC.h
+++ STDC.h
@@ -15,6 +15,13 @@
 #	define CONST const
 #	define STATIC(s) static s
 #endif /* __STDC_VERSION__ < 199901L */
+#ifdef __cplusplus
+#	define C_BEGIN extern "C" {
+#	define C_END }
+#else
+#	define C_BEGIN
+#	define C_END
+#endif /* __cplusplus */
 #define ARRSIZ(a) (sizeof(a)/sizeof*(a))
 
 #endif /* STDC_h */
